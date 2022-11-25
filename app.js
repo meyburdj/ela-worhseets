9920439f40965ddc4a486299ea04ab05a53fbed7
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2022-11-25 13:13:18 -0800

    added some exes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 //once user clicks to make question they can then do multiple choice with x answer choices or text box to write out answer in sentence form or paragraph form with or without lines
 //at end they click "generate" and it makes a downloadable pdf
 
-const $worksheetText = $("#worksheet-text");
+const $worksheetContainer = $("#worksheet-container");
 let $rawText = $("#text-input").val();
 
 
@@ -18,7 +18,6 @@ let textArray = $rawText.split("");
 
 $("#text-input-btn").on("click", function (evt) {
     evt.preventDefault();
-    $("#input-methods").addClass("d-none");
     let $rawText = $("#text-input").val();
 
     let lineCount = 1
@@ -27,28 +26,49 @@ $("#text-input-btn").on("click", function (evt) {
 
     while (paragraphArrayCount < paragraphArray.length) {
 
-        $worksheetText.append($("<div>", {
+        $worksheetContainer.append($("<div>", {
+            class: "row text-row"
+        }))
+        $(".text-row").last().append($("<div>", {
             text: `${lineCount}`,
             class: "col-1",
             contenteditable: "true"
         }))
-        $worksheetText.append($("<div>", {
+        $(".text-row").last().append($("<div>", {
             text: paragraphArray[paragraphArrayCount],
-            class: "col-11",
+            class: "col-10",
             contenteditable: "true"
         }))
-        $worksheetText.append($("<div>", {
-            text: `&nbsp`,
+        $(".text-row").last().append($("<i>", {
+            class: "col-1 bi bi-x btn close-btn",
+
+        }))
+
+        $(".text-row").last().append($("<div>", {
+            html: "&nbsp",
             class: "col-12",
-            css: {
-                "color": "white"
-            }
         }))
 
         lineCount++
         paragraphArrayCount++;
     }
+    $worksheetContainer.append($("<div>", {
+        class: "row questions-print-btns"
+    }))
 
+    $(".questions-print-btns").append($("<button>", {
+        class: "col-3 btn btn-primary border",
+        text: "Create multiple choice question"
+    }))
+    $(".questions-print-btns").append($("<button>", {
+        class: "col-3 btn btn-primary border",
+        text: "Create short response question"
+    }))
+    $(".questions-print-btns").append($("<button>", {
+        class: "btn btn-info col-3 border",
+        text: "Print Worksheet"
+        // text: "Create short response question"
+    }))
 
 })
 
@@ -60,6 +80,9 @@ $("#url-api-select").on("click", function () {
     $("#url-api-method").removeClass("d-none")
 })
 
+$worksheetContainer.on("click", ".close-btn", function (evt) {
+    $(evt.target).parent().remove();
+})
 
 
 
